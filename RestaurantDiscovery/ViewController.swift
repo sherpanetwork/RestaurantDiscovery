04439427fd38d0ed0f3d7e293e5623807654f059
Riley Hooper <ffhooper@churchofjesuschrist.org>
2021-10-13 19:29:13 -0600

    Initial Commit

---
+++ RestaurantDiscovery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestaurantDiscovery
+//
+//  Created by Riley Hooper on 10/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
